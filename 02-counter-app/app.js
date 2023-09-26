206f192dbb97d73502ab23d7ff55210f8d404342
Osman Can Durdu <osmandurduu74@hotmail.com>
2023-09-26 14:17:19 +0300

    computeds

--- 02-counter-app/app.js
+++ 02-counter-app/app.js
@@ -2,7 +2,29 @@ const app = Vue.createApp({
     data(){
         return{
             counter:0,
+            counter2:0,
         };
+    },
+    methods:{
+       
+    },
+    computed : {
+        getCounterResult(){
+            console.log("counter 1 çalıştı");
+            return this.counter >= 5 ? 'BÜYÜK' :  'KÜÇÜK';
+        },
+        getCounter2Result(){
+            console.log("counter 2 çalıştı");
+            return this.counter2 >= 5 ? 'BÜYÜK' :  'KÜÇÜK';
+        },
+    },
+    watch :{
+        counter(newValue, oldValue){
+            console.log("Counter", oldValue, "==>", newValue);
+        },
+        getCounterResult(newValue, oldValue){
+            console.log("RESULT", oldValue, "==>", newValue);
+        }
     },
   /*  methods:{
         inc(){
