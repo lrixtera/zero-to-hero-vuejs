0295b2e37222ca69ef2fc64c750333a9e859b912
Osman Can Durdu <osmandurduu74@hotmail.com>
2023-09-27 17:40:03 +0300

    todo list app

--- 07-loops/app.js
+++ 07-loops/app.js
@@ -1,12 +1,27 @@
 const app = Vue.createApp({
     data(){
         return{
-            todoList: ["Todo 1","Todo 2", "Todo 3", "Todo 4 "],
+            todoList: [
+                {id:1 , text:"Vue Bootcamp Gün 1",completed:false},
+                {id:2 , text:"Vue Bootcamp Gün 2",completed:false},
+                {id:3 , text:"Vue Bootcamp Gün 3",completed:false},
+                {id:4 , text:"Vue Bootcamp Gün 4",completed:false},
+                {id:5 , text:"Vue Bootcamp Gün 5",completed:false},
+                {id:6 , text:"Vue Bootcamp Gün 6",completed:false}
+            ],
         }
     },
     methods: {
-        addTodo(){
-            this.todoList.push($event.target.value);
-        }
+        addTodo(event){
+            this.todoList.push({
+                id: new Date().getTime(),
+                text: event.target.value,
+                completed: false,
+            });
+            
+        },
+        removeItem(todoItem){
+            this.todoList =this.todoList.filter(todo=> todo !== todoItem)
+        },
     },
 }).mount("#app");
