63bc0782c7a9dc1927b7702bb25585f4d8212a26
Osman Can Durdu <osmandurduu74@hotmail.com>
2023-09-29 18:05:53 +0300

    some fixes

--- 11-components-communications/src/components/UserSection.vue
+++ 11-components-communications/src/components/UserSection.vue
@@ -10,7 +10,6 @@
 import ListSection from "@/components/ListSection"
 import AddSection from "@/components/AddSection"
 export default{
-    
    // props:["userList"],
     emits:["new-item"],
     components:{
