378d3b3169ba642ddf5b076cea3b876106c88555
Osman Can Durdu <osmandurduu74@hotmail.com>
2023-10-02 10:24:38 +0300

    component-slots

---
+++ 12-component-slots/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MeinModal :onSave="onSave" :title="'ModalTitle'" :content="content"/>
+</template>
+
+<script>
+import MeinModal from '@/components/MeinModal.vue'
+
+export default {
+  components: {
+    MeinModal
+  },
+  data(){
+    return{
+      content: `<h3 class="text-red">Title</h3>`
+    }
+  },
+  methods:{
+    onSave(){
+      alert("Saved");
+    }
+  }
+}
+</script>
+
+
---
+++ 12-component-slots/src/components/MeinModal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <h1>{{ title }}</h1>
+        </div>
+        <hr>
+        <div class="content my-2" v-html="content">
+            
+        </div>
+        <hr>
+        <div class="footer text-right">
+            <button class="mr-2">Kapat</button>
+            <button class="ml-2 green" @click="onSave">kaydet</button>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    props:["title","content","onSave"]
+}
+</script>
